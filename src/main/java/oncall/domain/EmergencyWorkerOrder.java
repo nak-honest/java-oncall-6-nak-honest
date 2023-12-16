2269a1f2a16099e2f27ca2b7aa809ae89bba044d
nhlee98 <e5slnh9876@gmail.com>
2023-12-16 15:20:19 +0900

    feat: 비상 근무 순서의 이름에 중복된 이름이 있다면 예외를 발생시킨다

--- src/main/java/oncall/domain/EmergencyWorkerOrder.java
+++ src/main/java/oncall/domain/EmergencyWorkerOrder.java
@@ -17,12 +17,19 @@ public final class EmergencyWorkerOrder {
 
     private void validate() {
         validateWorkerCount();
+        validateDuplicateWorker();
     }
 
     private void validateWorkerCount() {
         if (workers.size() < MIN_WORKER_COUNT || workers.size() > MAX_WORKER_COUNT) {
             throw new IllegalArgumentException(
                     String.format("[ERROR] 근무자는 %d명 이상 %d명 이하로 입력해주세요.", MIN_WORKER_COUNT, MAX_WORKER_COUNT));
+        }
+    }
+
+    private void validateDuplicateWorker() {
+        if (workers.stream().distinct().count() != workers.size()) {
+            throw new IllegalArgumentException("[ERROR] 근무자의 이름은 중복될 수 없습니다.");
         }
     }
 
--- src/main/java/oncall/domain/Worker.java
+++ src/main/java/oncall/domain/Worker.java
@@ -1,5 +1,7 @@
 package oncall.domain;
 
+import java.util.Objects;
+
 public class Worker {
     private static final int MIN_NAME_LENGTH = 1;
     private static final int MAX_NAME_LENGTH = 5;
@@ -16,5 +18,22 @@ public class Worker {
             throw new IllegalArgumentException(
                     String.format("[ERROR] 이름은 %d자 이상 %d자 이하로 입력해주세요.", MIN_NAME_LENGTH, MAX_NAME_LENGTH));
         }
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (!(object instanceof Worker worker)) {
+            return false;
+        }
+
+        return name.equals(worker.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }

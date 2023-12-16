753c4aa2417a3e1649e7d30dc6a84c7df894fda2
nhlee98 <e5slnh9876@gmail.com>
2023-12-16 14:11:51 +0900

    style: 테스트 메소드명 오타 수정

--- src/test/java/oncall/domain/MonthTest.java
+++ src/test/java/oncall/domain/MonthTest.java
@@ -9,7 +9,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 public class MonthTest {
     @ParameterizedTest
     @ValueSource(ints = {0, -1, 13})
-    void 메인_메뉴가_1_또는_Q가_아니라면_예외를_발생시킨다(int month) {
+    void 월이_1에서_12_사이의_숫자가_아니라면_예외를_발생시킨다(int month) {
         assertThatThrownBy(() -> Month.of(month))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 존재하지 않는 월입니다.");
@@ -17,7 +17,7 @@ public class MonthTest {
 
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3, 4, 5, 6, 7, 8, 9 ,10, 11, 12})
-    void 메인_메뉴가_1_또는_Q라면_예외를_발생시키지_않는다(int month) {
+    void 월이_1에서_12_사이의_숫자라면_예외를_발생시키지_않는다(int month) {
         assertThatCode(() -> Month.of(month))
                 .doesNotThrowAnyException();
     }

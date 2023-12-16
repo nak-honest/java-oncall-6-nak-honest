4b75e7a1e7d296415e9586a70ed5a16f8912f53c
nhlee98 <e5slnh9876@gmail.com>
2023-12-16 16:48:32 +0900

    feat: 비상 근무를 배정할 월과 시작 요일을 입력 받는다

---
+++ src/main/java/oncall/controller/EmergencyWorkdaysController.java
@@ -0,0 +1,23 @@
+package oncall.controller;
+
+import oncall.domain.date.Dates;
+import oncall.dto.WorkMonthDto;
+import oncall.service.EmergencyWorkdaysService;
+import oncall.view.InputView;
+import oncall.view.OutputView;
+
+public class EmergencyWorkdaysController {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final EmergencyWorkdaysService emergencyWorkdaysService;
+
+    public EmergencyWorkdaysController(InputView inputView, OutputView outputView, EmergencyWorkdaysService emergencyWorkdaysService) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.emergencyWorkdaysService = emergencyWorkdaysService;
+    }
+
+    public void run() {
+        Dates workDates = emergencyWorkdaysService.getWorkDates(inputView.readWorkMonth());
+    }
+}
--- src/main/java/oncall/service/EmergencyWorkdaysService.java
+++ src/main/java/oncall/service/EmergencyWorkdaysService.java
@@ -1,2 +1,24 @@
-package oncall.service;public class EmergencyWorkdaysService {
+package oncall.service;
+
+import oncall.domain.EmergencyWorkdaysSchedule;
+import oncall.domain.EmergencyWorkdaysScheduler;
+import oncall.domain.date.DateFactory;
+import oncall.domain.date.Dates;
+import oncall.domain.date.DayOfWeek;
+import oncall.domain.date.Month;
+import oncall.dto.WorkMonthDto;
+
+public class EmergencyWorkdaysService {
+    private final EmergencyWorkdaysScheduler emergencyWorkdaysScheduler;
+
+    public EmergencyWorkdaysService(EmergencyWorkdaysScheduler emergencyWorkdaysScheduler) {
+        this.emergencyWorkdaysScheduler = emergencyWorkdaysScheduler;
+    }
+
+    public Dates getWorkDates(WorkMonthDto workMonthDto) {
+        Month month = Month.of(workMonthDto.getMonth());
+        DayOfWeek startDayOfWeek = DayOfWeek.of(workMonthDto.getStartDayOfWeek());
+
+        return DateFactory.createDates(month, startDayOfWeek);
+    }
 }

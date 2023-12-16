c9e2e881d926027cc04040bfe77c017b933b6c11
nhlee98 <e5slnh9876@gmail.com>
2023-12-16 14:13:42 +0900

    feat: 시작 요일이 월, 화, 수, 목, 금, 토, 일 중 하나가 아니라면 예외를 발생시킨다

---
+++ src/main/java/oncall/domain/DayOfWeek.java
@@ -0,0 +1,26 @@
+package oncall.domain;
+
+import java.util.Arrays;
+
+public enum DayOfWeek {
+    MONDAY("월"),
+    TUESDAY("화"),
+    WEDNESDAY("수"),
+    THURSDAY("목"),
+    FRIDAY("금"),
+    SATURDAY("토"),
+    SUNDAY("일");
+
+    private String value;
+
+    DayOfWeek(String value) {
+        this.value = value;
+    }
+
+    public static DayOfWeek of(String value) {
+        return Arrays.stream(values())
+                .filter(dayOfWeek -> dayOfWeek.value.equals(value))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 존재하지 않는 요일입니다."));
+    }
+}
